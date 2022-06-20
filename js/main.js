13415eaaa969d25fe057a3efbbe76b9d7121c3a0
Letendart Olivia <olivia.letendart@socrate-edu.com>
2022-06-20 16:41:19 +0200

    fix(feature): fix and create new functions and remove html tag in the index.html + integrate new functions in the file search.js

--- js/main.js
+++ js/main.js
@@ -1,49 +1,55 @@
 // DOM Elements
 const searchbar = document.querySelector('input[name="input-search"]');
+const searchIngredients = document.querySelector('#input-ingredients');
+const searchAppliance = document.querySelector('#input-appliance');
+const searchUstensils = document.querySelector('#input-ustensils');
+const idTag = document.querySelector('#tag');
 
+const feature = features(recipes);
+feature.displayRecipes(feature.recipes)
 
 // Searchbar
 searchbar.addEventListener("keyup", () => {
     const searchbarValue = searchbar.value;
     if (searchbarValue.length > 2) {
-        let result = recipes.filter(recipe => recipe.name.toLowerCase().search(searchbarValue.toLowerCase()) !== -1);
-        Recipe.initialise();
-        searchRecipe(result);
+        feature.result = feature.searchBar(searchbarValue);
     } else {
-        Recipe.initialise();
-        searchRecipe(recipes);
+        feature.displayRecipes(feature.recipes);
     }
 });
 
-Recipe.initialise();
+// Dropdown Search
+searchIngredients.addEventListener('keyup', (e) => {
+        if (e.target.value.length > 0) {
+            feature.searchDropdown(e.target.value);
+        } else {
+            feature.displayRecipes(feature.recipes)
+        }
+    }
+);
 
-const searchRecipe = (recipes) => {
-    recipes.forEach((data) => {
-        const recipe = new Recipe(
-            data.name,
-            data.time,
-            data.ingredients,
-            data.description,
-            data.appliance,
-            data.ustensils
-        );
+searchAppliance.addEventListener('keyup', (e) => {
+    if(e.target.value.length > 0) {
+        feature.searchDropdownAppliance(e.target.value)
+    } else {
+       feature.displayRecipes(feature.recipes)
+    }
+})
 
-        recipe.showCart();
-    });
-
-
-    Recipe.listElements(Recipe.ingredientsArray, Recipe.ingredientsDropdown);
-    Recipe.listElements(Recipe.applianceArray, Recipe.applianceDropdown);
-    Recipe.listElements(Recipe.ustensilsArray, Recipe.ustensilsDropdown);
-}
-
-searchRecipe(recipes);
+searchUstensils.addEventListener('keyup', (e) => {
+    if(e.target.value.length > 0) {
+        feature.searchDropdownUstensils(e.target.value);
+    } else {
+        feature.displayRecipes(feature.recipes)
+    }
+})
 
 let buttonDropdown = document.querySelectorAll('.t-select-btn');
 
 const openDropdown = (event, index) => {
     let listDropdown = document.querySelectorAll('.t-dropdown-list');
     let divDropdown = document.querySelectorAll('.t-dropdown-select');
+    let widthDropdown = document.querySelectorAll('.t-dropdown-block');
     let indexDrop = index;
 
     listDropdown.forEach((drop, index) => {
@@ -51,10 +57,12 @@ const openDropdown = (event, index) => {
             drop.classList.toggle('active');
             buttonDropdown[indexDrop].classList.toggle('rotate-active');
             divDropdown[indexDrop].classList.toggle('borderRadius-active');
+            widthDropdown[indexDrop].classList.toggle('width-active');
         } else {
             drop.classList.remove('active');
             buttonDropdown[index].classList.remove('rotate-active');
             divDropdown[index].classList.remove('borderRadius-active');
+            widthDropdown[index].classList.remove('width-active');
         }
     });
 }
